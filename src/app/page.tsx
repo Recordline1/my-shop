d70064915c6c057cfbdd34d8ad9327f63e99e301
Recordline1 <super-dudi@ukr.net>
2026-06-28 19:17:12 +0300

    fix: update shop title from

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default async function Home() {
 
   return (
     <main className="container mx-auto p-8">
-      <h1 className="text-3xl font-bold mb-8">Мой магазин</h1>
+      <h1 className="text-3xl font-bold mb-8">My Shop</h1>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {products?.map((product: any) => (
